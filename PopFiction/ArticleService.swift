38257d0366138348b8774243f2489733699d3c19
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-08-29 18:57:24 +0300

    ArticleService stub-body added

---
+++ PopFiction/ArticleService.swift
@@ -0,0 +1,19 @@
+//
+//  ArticleService.swift
+//  PopFiction
+//
+//  Created by Богдан Ткаченко on 8/29/19.
+//  Copyright © 2019 Богдан Ткаченко. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class ArticleService {
+    
+    public static let shared = ArticleService()
+    
+    
+    
+}
