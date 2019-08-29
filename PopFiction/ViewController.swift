c7df22b1c1e25ad115169690582f40bda582b097
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-08-29 17:30:08 +0300

    Initial Commit

---
+++ PopFiction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopFiction
+//
+//  Created by Богдан Ткаченко on 8/29/19.
+//  Copyright © 2019 Богдан Ткаченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
