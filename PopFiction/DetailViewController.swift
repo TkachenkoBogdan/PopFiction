bee8d2233577c2c762e3898fff7b58cdc1e16797
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-08-30 14:30:54 +0300

    Basis scenes hierarchy has been laid out in storyboards

---
+++ PopFiction/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  PopFiction
+//
+//  Created by Богдан Ткаченко on 8/30/19.
+//  Copyright © 2019 Богдан Ткаченко. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
