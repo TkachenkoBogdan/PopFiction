64b122af6c112ceb7cb5bfbeb5c19fed0f6d9aca
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-09-05 09:49:52 +0300

    small fixes

---
+++ PopFiction/AppConfig.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PopFiction
+//
+//  Created by Богдан Ткаченко on 8/29/19.
+//  Copyright © 2019 Богдан Ткаченко. All rights reserved.
+//
+
+import Foundation
+
+/// Configurations and keys for this app
+struct AppConfig {
+    /// API key for NYT API:
+  static let apiKey = "ZDGO1GbINnwODAofUGwMWYOeKl7GOT76"
+    
+}
+
+//Notifications:
+let dataSourceDidChangeNotification = Notification.Name(rawValue: "DataSourceDidChange")
